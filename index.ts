a423855947b7f33d9a55b4c36f277dbaaaa8fc4d
Yutaro Yasuda <yutaro.toro2023@gmail.com>
2024-03-03 01:30:14 +0900

    feat: HTTP/0.9時代のWebサーバの実装

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import { createServer, IncomingMessage, ServerResponse } from "http";
+
+// リクエストハンドラ関数に型を適用
+function handler(req: IncomingMessage, res: ServerResponse) {
+  const { method, url, headers } = req;
+  console.log(`${method} ${url}`);
+  console.log(headers);
+
+  // 簡単なHTMLレスポンスを送信
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<html><body>hello</body></html>\n');
+}
+
+// サーバーを作成し、ポート18888でリッスン
+const server = createServer(handler);
+server.listen(18888, () => {
+  console.log("start http listening : 18888");
+});
